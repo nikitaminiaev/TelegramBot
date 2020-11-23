54bd73bc1a51bb076e68da320bf905d2077974cf
n.Minayev <valitev@gmail.com>
2020-11-23 14:52:08 +0400

    Рабочий бот - попугай

---
+++ bot.py
@@ -0,0 +1,36 @@
+import os
+import aiohttp
+from dotenv import load_dotenv
+from aiogram import Bot, Dispatcher, executor, types
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+
+if os.path.exists(dotenv_path):
+    a = load_dotenv(dotenv_path)
+token = os.getenv('TOKEN')
+bot = Bot(token=token)
+dp = Dispatcher(bot)
+map = {'Вероника': 'Ути!!!',
+       'Никита': 'Котя!!!',
+       'Санек': 'харошь',
+       'Александр': 'харошь',
+       'Апрес': 'го в шахматы, я тебя дерну',
+       }
+
+@dp.message_handler(commands=['start'])
+async def process_start_command(message: types.Message):
+    await message.reply("Привет!\nКак тебя зовут?")
+
+@dp.message_handler(commands=['help'])
+async def process_help_command(message: types.Message):
+    await message.reply("Напиши мне что-нибудь, и я отпрпавлю этот текст тебе в ответ!")
+
+@dp.message_handler()
+async def say(message: types.Message):
+    if message.text in map or message.text == 'Александр':
+        await message.answer(map[message.text])
+    await message.answer(message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
