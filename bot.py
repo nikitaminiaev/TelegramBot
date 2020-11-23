020b2f3169a4c120ae95d9625793f727acb3040e
n.Minayev <valitev@gmail.com>
2020-11-23 17:52:14 +0400

    Refactoring

--- bot.py
+++ bot.py
@@ -10,25 +10,29 @@ if os.path.exists(dotenv_path):
 token = os.getenv('TOKEN')
 bot = Bot(token=token)
 dp = Dispatcher(bot)
-map = {'Вероника': 'Ути!!!',
+mapping = {
+       'Вероника': 'Ути!!!',
        'Никита': 'Котя!!!',
        'Санек': 'харошь',
        'Александр': 'харошь',
        'Апрес': 'го в шахматы, я тебя дерну',
        }
 
+
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
     await message.reply("Привет!\nКак тебя зовут?")
+
 
 @dp.message_handler(commands=['help'])
 async def process_help_command(message: types.Message):
     await message.reply("Напиши мне что-нибудь, и я отпрпавлю этот текст тебе в ответ!")
 
+
 @dp.message_handler()
 async def say(message: types.Message):
-    if message.text in map or message.text == 'Александр':
-        await message.answer(map[message.text])
+    if message.text in mapping:
+        await message.answer(mapping[message.text])
     await message.answer(message.text)
 
 
