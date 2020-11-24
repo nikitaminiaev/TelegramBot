c20aa081a0430acf36246dbeb3f6d0289d880332
n.Minayev <valitev@gmail.com>
2020-11-24 13:55:37 +0400

    Переделать с колбеком

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os
 import aiohttp
+import random
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, executor, types
 
@@ -10,18 +11,19 @@ if os.path.exists(dotenv_path):
 token = os.getenv('TOKEN')
 bot = Bot(token=token)
 dp = Dispatcher(bot)
-mapping = {
-       'Вероника': 'Ути!!!',
-       'Никита': 'Котя!!!',
-       'Санек': 'харошь',
-       'Александр': 'харошь',
-       'Апрес': 'го в шахматы, я тебя дерну',
-       }
 
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
-    await message.reply("Привет!\nКак тебя зовут?")
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton('Рандом')
+    item2 = types.KeyboardButton('Как дела?')
+    markup.add(item1, item2)
+
+    await bot.send_message(message.chat.id,
+                           "Добро пожаловать, {0.first_name}!\nЯ - <b>{1}</b>, бот.".format(message.from_user,
+                                                                                            bot.get('first_name')),
+                           parse_mode='html', reply_markup=markup)
 
 
 @dp.message_handler(commands=['help'])
@@ -31,9 +33,40 @@ async def process_help_command(message: types.Message):
 
 @dp.message_handler()
 async def say(message: types.Message):
-    if message.text in mapping:
-        await message.answer(mapping[message.text])
-    await bot.send_message(message.chat.id, message.text)
+    if message.chat.type == 'private':
+        if message.text == 'Рандом':
+            await bot.send_message(message.chat.id, str(random.randint(0, 100)))
+        elif message.text == 'Как дела?':
+
+            markup = types.InlineKeyboardMarkup(row_width=2)
+            item1 = types.InlineKeyboardButton("Хорошо", callback_data='good')
+            item2 = types.InlineKeyboardButton("Не очень", callback_data='bad')
+
+            markup.add(item1, item2)
+
+            await bot.send_message(message.chat.id, 'Отлично, сам как?', reply_markup=markup)
+        else:
+            await bot.send_message(message.chat.id, 'Я не знаю что ответить 😢')
+
+
+@dp.callback_query_handler(lambda call: True)
+async def callback_inline(call):
+    try:
+        if call.message:
+            if call.data == 'good':
+                await bot.send_message(call.message.chat.id, 'Вот и отличненько')
+            elif call.data == 'bad':
+                await bot.send_message(call.message.chat.id, 'Бывает')
+
+            await bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                        text="",
+                                        reply_markup=None)
+
+            await bot.answer_callback_query(callback_query_id=call.id, show_alert=False,
+                                            text="ЭТО ТЕСТОВОЕ УВЕДОМЛЕНИЕ!!11")
+
+    except Exception as e:
+        print(repr(e))
 
 
 if __name__ == '__main__':
